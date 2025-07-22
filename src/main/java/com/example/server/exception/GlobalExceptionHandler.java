1f9317ca3fb3dcbc0ba4625ae0377fe055d90ee0
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-07-22 23:55:54 +0300

    add exception handler

---
+++ src/main/java/com/example/server/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.server.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+}
